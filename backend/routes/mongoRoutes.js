4d48175f7a4c3c63afdfffd8bbeab5582fd72091
hiramrr <sasuke_77.23@hotmail.com>
2025-09-07 16:08:01 -0600

    otro pequeño error jaja

--- backend/routes/mongoRoutes.js
+++ backend/routes/mongoRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {
-    getRuta
-} from "../controllers/mongoControllers.js";
+import { getRuta } from "../controllers/mongoControllers.js";
 
 const router = express.Router();
 
 // Endpoints relativos a MongoDB
-router.get("/rutas", getRuta);//api/rutas
+router.get("/rutas", getRuta); //api/rutas
+
+router.get("/rutas/:id", getRutaById);
 
 export default router;
