f22e0f71a6e29d4b70911f188df02e35dbaf409f
hiramrr <sasuke_77.23@hotmail.com>
2025-09-09 09:57:43 -0600

    Se agrego la funcion para obtener los datos de todas las rutas

--- backend/controllers/mongoControllers.js
+++ backend/controllers/mongoControllers.js
@@ -22,29 +22,57 @@ export const getRuta = async (req, res) => {
 
 export const autocomplete = async (req, res) => {
   try {
-    const query = req.query.query || '';
+    const query = req.query.query || "";
     if (!query) {
-      return res.status(400).json({ error: 'Se requiere el parámetro query' });
+      return res.status(400).json({ error: "Se requiere el parámetro query" });
     }
 
     const db = await connectDB(); // 👈 conexión reutilizable
-    const collection = db.collection('geocodificacion');
+    const collection = db.collection("geocodificacion");
 
-    const results = await collection.find(
-      { nombre: { $regex: query, $options: 'i' } },
-      { projection: { nombre: 1, location: 1 } }
-    ).limit(10).toArray();
+    const results = await collection
+      .find(
+        { nombre: { $regex: query, $options: "i" } },
+        { projection: { nombre: 1, location: 1 } },
+      )
+      .limit(10)
+      .toArray();
 
     // Imprimir coordenadas en consola
-    results.forEach(r => {
+    results.forEach((r) => {
       if (r.location?.coordinates) {
-        console.log(`Lugar: ${r.nombre}, Coordenadas: ${r.location.coordinates}`);
+        console.log(
+          `Lugar: ${r.nombre}, Coordenadas: ${r.location.coordinates}`,
+        );
       }
     });
 
     res.json(results);
   } catch (error) {
     console.error("Error en autocomplete:", error);
-    res.status(500).json({ error: 'Error en el servidor' });
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+};
+
+export const getAllRutas = async (req, res) => {
+  try {
+    const db = await connectDB();
+    const options = {
+      projection: {
+        name: 1,
+        desc: 1,
+        notes: 1,
+        id: 1,
+        peak_am: 1,
+        midday: 1,
+        peak_pm: 10,
+        night: 1,
+      },
+    };
+    const rutas = await db.collection("rutas").find({}, options).toArray();
+    res.json(rutas);
+  } catch (error) {
+    console.error("Error al obtener todas las rutas", error);
+    res.status(500).json({ error: "Error al obtener las rutas" });
   }
 };
