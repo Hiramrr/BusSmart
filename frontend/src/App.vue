25a43a5357f4017696a51721d0f12ac98d75cca1
Rodri <zS23014183@estudiantes.uv.mx>
2025-09-10 10:04:49 -0600

    componiendo root

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <TheHeader />
-
   <main>
     <RouterView />
   </main>
   
-  <TheFooter />
 </template>
 
 <script setup>
 // Importas los componentes que uses aquí
-import TheHeader from './components/layout/TheHeader.vue'
-import TheFooter from './components/layout/TheFooter.vue'
 import { RouterView } from 'vue-router'
 </script>
 <style scoped>
