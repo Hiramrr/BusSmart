f418cfe18822a24f495ac496537d8a87734d5bac
AlfredoAta <alfredoans946@gmail.com>
2025-09-10 09:52:17 -0600

    Mis cambios en index.html

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,21 +8,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
     }
-    // {
-    //   path: '/mapa',
-    //   name: 'mapa',
-    //   // Esta es una forma de "carga diferida" (lazy-loading).
-    //   // El código de MapaView solo se descarga cuando el usuario visita /mapa.
-    //   // ¡Es una buena práctica para optimizar la carga inicial de tu app!
-    //   component: () => import('../views/MapaView.vue')
-    // },
-    // {
-    //   path: '/login',
-    //   name: 'login',
-    //   // También usamos carga diferida para la vista de login.
-    //   component: () => import('../views/LoginView.vue')
-    // }
   ]
 })
 
