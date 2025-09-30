de51103facd093040b2bebf74ceff3ba2899a003
hiramrr <sasuke_77.23@hotmail.com>
2025-09-30 08:54:57 -0600

    Se creo la pagina para subir los datos de las rutas

---
+++ frontend/src/components/admin/FormularioRuta.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Crear una nueva ruta</h1>
+  <form>
+    <ul>
+      <li>
+        <label for="nombre">Nombre de la ruta</label>
+        <input type="text" id="nombre" name="nombre_ruta" />
+      </li>
+      <li>
+        <label for="id">ID de la ruta</label>
+        <input type="numer" id="id" name="id_ruta" />
+      </li>
+      <li>
+        <label for="peak_am">Hora de salida</label>
+        <input type="number" id="salida" name="salida_ruta" />
+      </li>
+      <li>
+        <label for="peak_pm">Ultima hora de salida</label>
+        <input type="number" id="salida" name="salida_ruta" />
+      </li>
+      <li>
+        <label for="foto">Foto del autobus</label>
+        <input type="file" id="foto" name="foto" accept="image/png, image/jpeg" />
+      </li>
+      <li>
+        <label for="geojson">GeoJson de la ruta</label>
+        <input
+          type="file"
+          id="GeoJson"
+          name="GeoJson"
+          accept=".geojson, .json, application/geo+json"
+        />
+      </li>
+      <li>
+        <label for="geojson">GeoJson de la ruta</label>
+        <input
+          type="file"
+          id="GeoJson"
+          name="GeoJson"
+          accept=".geojson, .json, application/geo+json"
+        />
+        <a href="<<!nav>>geojson.io<<!/nav>>" target="_blank">
+          <button>O crear un geojson en geojson.io</button>
+        </a>
+      </li>
+    </ul>
+  </form>
+</template>
